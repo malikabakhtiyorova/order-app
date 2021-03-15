b0f965d0aadf2497dc89453843b7207dbad3a790
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-03-15 20:37:30 +0500

    data modeling

--- data-model.sql
+++ data-model.sql
@@ -9,3 +9,17 @@ create table orders (
     order_contact varchar(16),
     order_date date default current_date
 );
+
+insert into orders(order_fullname, order_gender, order_contact) values('asmo','f', '123');
+insert into orders(order_fullname, order_gender, order_contact) values('mas','f', '123');
+insert into orders(order_fullname, order_gender, order_contact, order_date) values('edh','m', '43', '2021-03-10');
+insert into orders(order_fullname, order_gender, order_contact, order_date) values('ewry','m', '34', '2021-03-10');
+
+select 
+    order_fullname,
+    order_gender,
+    order_contact
+ from orders
+ where 
+to_char( order_date, 'YYYY-MM-DD') =  to_char(now(), 'YYYY-MM-DD');
+
